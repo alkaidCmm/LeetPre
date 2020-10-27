076747593406b009a116d98681f39b8705db4e9a
蔡明明 <caimingming02@meituan.com>
2020-10-27 10:04:45 +0800

    Dfix

---
+++ src/com/cmm/leedcode/tree/medium/_103zigzagLevelOrder/Solution.java
@@ -0,0 +1,62 @@
+package com.cmm.leedcode.tree.medium._103zigzagLevelOrder;
+
+import com.cmm.leedcode.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author caimingming02
+ * @date 2020/10/20 5:45 PM
+ * @desc Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its zigzag level order traversal as:
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ */
+public class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if(level % 2 == 0){
+                    tmp.add(node.val);
+                }else{
+                    tmp.add(0,node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+            }
+            level++;
+            ret.add(tmp);
+        }
+        return ret;
+    }
+}
